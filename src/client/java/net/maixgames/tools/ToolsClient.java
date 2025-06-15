04b3acc8de9cf466a4bb7635db6a686c50968869
maxigameshd22 <maxigameshd22@gmail.com>
2025-06-15 12:00:58 +0200

    Initial commit

---
+++ src/client/java/net/maixgames/tools/ToolsClient.java
@@ -0,0 +1,10 @@
+package net.maixgames.tools;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ToolsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
